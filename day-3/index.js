4bded8d4af74db95751b69c67efada0f3c8931d1
Dipen Shah <dipen.shah@vayana.com>
2022-12-03 17:48:05 +0530

    Javascriptmas - Day 2 and Day 3

---
+++ day-3/index.js
@@ -0,0 +1,31 @@
+/* Challenge 1: Favorite Foods
+- Topic: Object Destructuring
+1. Edit the faveFoods object so that it contains 
+your favorite foods.
+2. Destructure the faveFoods object into three consts: 
+breakfast, lunch, and supper.
+3. Fetch the meals <section> from the DOM.
+4. Set the innerHTML content of the meals <section> to a paragraph
+that states what your favorite foods are for breakfast, lunch, and supper.
+Use a template literal to construct the string.
+
+E.g.
+For breakfast, I only like croissants 🥐. For lunch, I love pasta 🍝, 
+and for supper I want usually want pizza 🍕.
+*/
+
+const faveFoods = {
+  breakfast: "croissants 🥐",
+  lunch: "pasta 🍝",
+  supper: "pizza 🍕",
+};
+
+const setMealsData = () => {
+  const { breakfast, lunch, supper } = faveFoods;
+  const mealData = `For breakfast, I only like ${breakfast}. For lunch, I love ${lunch}, 
+  and for supper I want usually want ${supper}.`;
+  console.log(mealData);
+  document.querySelector("#meals").innerHTML = mealData;
+};
+
+setMealsData();
